d2d9cee99b5bf83f65d54040743b39b8cef9ef79
mTextures <fsword88@gmail.com>
2024-04-17 15:38:03 +0300

    first commit

---
+++ bebebeb.py
@@ -0,0 +1,16 @@
+
+for row in range(21):
+  for col in range(51):
+      if row  == 9:
+        print('-',end = "")
+      elif col == 25:
+        print('|', end = '')
+      else:
+        print(' ', end = '')
+  print() 
+print('exec')
+
+
+
+
+
---
+++ print ('гавно').py
@@ -0,0 +1,9 @@
+# Пример:
+# Ввели 5, ответ 5
+# Ввели −7, ответ 7
+num = int(input("Введите число: "))
+if num > 0:
+    print(num)
+else:
+    num = num*(-1)
+    print (num)
